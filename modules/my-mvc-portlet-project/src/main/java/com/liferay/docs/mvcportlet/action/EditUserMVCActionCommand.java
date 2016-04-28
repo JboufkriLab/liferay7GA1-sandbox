f66c453019679e79fa31b05c02f00be5e1730d58
Daniele Baggio <daniele.baggio@sysdata.it>
2016-04-28 15:57:02 +0200

    fix build , deploy, and runtime exec for new "my account" module

--- modules/my-mvc-portlet-project/src/main/java/com/liferay/docs/mvcportlet/action/EditUserMVCActionCommand.java
+++ modules/my-mvc-portlet-project/src/main/java/com/liferay/docs/mvcportlet/action/EditUserMVCActionCommand.java
@@ -14,23 +14,144 @@
 
 package com.liferay.docs.mvcportlet.action;
 
-import org.osgi.service.component.annotations.Component;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import com.liferay.announcements.kernel.service.AnnouncementsDeliveryLocalService;
 import com.liferay.docs.mvcportlet.MyMvcPortletPortletKeys;
+import com.liferay.document.library.kernel.service.DLAppLocalService;
+import com.liferay.portal.kernel.exception.UserPasswordException;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.CompanyConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.Authenticator;
+import com.liferay.portal.kernel.service.ListTypeLocalService;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 /**
  */
-@Component(
-	immediate = true,
-	property = {
-		"javax.portlet.name=" + MyMvcPortletPortletKeys.MY_ACCOUNT,
-		"mvc.command.name=updateUser"
-	},
-	service = MVCActionCommand.class
-)
-public class EditUserMVCActionCommand extends
-	com.liferay.my.account.web.portlet.action.EditUserMVCActionCommand {
+@Component(immediate = true, property = {
+	"javax.portlet.name=" + MyMvcPortletPortletKeys.MY_ACCOUNT,
+	"mvc.command.name=updateUser"
+}, service = MVCActionCommand.class)
+public class EditUserMVCActionCommand
+	extends
+	com.liferay.users.admin.web.portlet.action.EditUserMVCActionCommand {
 
+	@Override
+	protected void doProcessAction(
+		ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
 
+		if (redirectToLogin(actionRequest, actionResponse)) {
+			return;
+		}
+
+		super.doProcessAction(actionRequest, actionResponse);
+	}
+
+	@Override
+	protected Object[] updateUser(
+		ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String currentPassword = actionRequest
+			.getParameter("password0");
+		String newPassword = actionRequest
+			.getParameter("password1");
+
+		User user = PortalUtil
+			.getSelectedUser(actionRequest);
+
+		if (Validator
+			.isNotNull(currentPassword)) {
+			if (Validator
+				.isNull(newPassword)) {
+				throw new UserPasswordException.MustNotBeNull(user
+					.getUserId());
+			}
+
+			Company company = PortalUtil
+				.getCompany(actionRequest);
+
+			String authType = company
+				.getAuthType();
+
+			Map<String, String[]> headerMap = new HashMap<>();
+			Map<String, String[]> parameterMap = new HashMap<>();
+			Map<String, Object> resultsMap = new HashMap<>();
+
+			int authResult = Authenticator.FAILURE;
+
+			if (authType
+				.equals(CompanyConstants.AUTH_TYPE_EA)) {
+				authResult = userLocalService
+					.authenticateByEmailAddress(company
+						.getCompanyId(), user
+							.getEmailAddress(),
+						currentPassword, headerMap, parameterMap, resultsMap);
+			}
+			else if (authType
+				.equals(CompanyConstants.AUTH_TYPE_ID)) {
+				authResult = userLocalService
+					.authenticateByUserId(company
+						.getCompanyId(), user
+							.getUserId(),
+						currentPassword, headerMap, parameterMap, resultsMap);
+			}
+			else if (authType
+				.equals(CompanyConstants.AUTH_TYPE_SN)) {
+				authResult = userLocalService
+					.authenticateByScreenName(company
+						.getCompanyId(), user
+							.getScreenName(),
+						currentPassword, headerMap, parameterMap, resultsMap);
+			}
+
+			if (authResult == Authenticator.FAILURE) {
+				throw new UserPasswordException.MustMatchCurrentPassword(user
+					.getUserId());
+			}
+		}
+		else if (Validator
+			.isNotNull(newPassword)) {
+			throw new UserPasswordException.MustNotBeNull(user
+				.getUserId());
+		}
+
+		return super.updateUser(actionRequest, actionResponse);
+	}
+
+	@Reference(unbind = "-")
+	protected void setListTypeLocalService(
+		ListTypeLocalService listTypeLocalService) {
+
+		super.setListTypeLocalService(listTypeLocalService);
+	}
+
+	@Reference(unbind = "-")
+	protected void setUserService(UserService userService) {
+		super.setUserService(userService);
+	}
+
+	@Reference(unbind = "-")
+	protected void setDLAppLocalService(DLAppLocalService dlAppLocalService) {
+		super.setDLAppLocalService(dlAppLocalService);
+	}
+
+	@Reference(unbind = "-")
+	protected void setAnnouncementsDeliveryLocalService(
+		AnnouncementsDeliveryLocalService announcementsDeliveryLocalService) {
+
+		super.setAnnouncementsDeliveryLocalService(announcementsDeliveryLocalService);
+	}
 }
